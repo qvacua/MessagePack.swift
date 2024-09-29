397d790bbe50cdcc62d97ef7d11292c24260507d
Tae Won Ha <h@taewon.de>
2024-09-29 17:53:04 +0200

    Migrate to Testing

--- Tests/MessagePackTests/ArrayTests.swift
+++ Tests/MessagePackTests/ArrayTests.swift
@@ -1,41 +1,42 @@
 import Foundation
 @testable import MessagePack
 import XCTest
+import Testing
 
-class ArrayTests: XCTestCase {
-  func testLiteralConversion() {
+struct ArrayTests {
+  @Test func testLiteralConversion() {
     let implicitValue: MessagePackValue = [0, 1, 2, 3, 4]
     let payload: [MessagePackValue] = [.uint(0), .uint(1), .uint(2), .uint(3), .uint(4)]
-    XCTAssertEqual(implicitValue, .array(payload))
+    #expect(implicitValue == .array(payload))
   }
 
-  func testPackFixarray() {
+  @Test func testPackFixarray() {
     let value: [MessagePackValue] = [.uint(0), .uint(1), .uint(2), .uint(3), .uint(4)]
     let packed = Data([0x95, 0x00, 0x01, 0x02, 0x03, 0x04])
-    XCTAssertEqual(pack(.array(value)), packed)
+    #expect(pack(.array(value)) == packed)
   }
 
-  func testUnpackFixarray() {
+  @Test func testUnpackFixarray() {
     let packed = Data([0x95, 0x00, 0x01, 0x02, 0x03, 0x04])
     let value: [MessagePackValue] = [.uint(0), .uint(1), .uint(2), .uint(3), .uint(4)]
 
     let unpacked = try? unpack(packed)
-    XCTAssertEqual(unpacked?.value, .array(value))
-    XCTAssertEqual(unpacked?.remainder.count, 0)
+    #expect(unpacked?.value == .array(value))
+    #expect(unpacked?.remainder.count == 0)
   }
 
-  func testPackArray16() {
+  @Test func testPackArray16() {
     let value = [MessagePackValue](repeating: nil, count: 16)
     let packed = Data([0xDC, 0x00, 0x10] + [UInt8](repeating: 0xC0, count: 16))
-    XCTAssertEqual(pack(.array(value)), packed)
+    #expect(pack(.array(value)) == packed)
   }
 
-  func testUnpackArray16() {
+  @Test func testUnpackArray16() {
     let packed = Data([0xDC, 0x00, 0x10] + [UInt8](repeating: 0xC0, count: 16))
     let value = [MessagePackValue](repeating: nil, count: 16)
 
     let unpacked = try? unpack(packed)
-    XCTAssertEqual(unpacked?.value, .array(value))
-    XCTAssertEqual(unpacked?.remainder.count, 0)
+    #expect(unpacked?.value == .array(value))
+    #expect(unpacked?.remainder.count == 0)
   }
 }
